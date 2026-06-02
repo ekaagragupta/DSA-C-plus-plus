526a79f17162715bfec547949472607dc5e03b1d
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-06-02 14:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.24%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int current_sum = 0;
+        int max_sum = nums[0];
+        for (int i = 0; i < nums.size(); i++) {
+            current_sum += nums[i];
+            max_sum = max(current_sum, max_sum);
+
+            if(current_sum<0){
+                current_sum=0;
+            }
+        }
+          return max_sum;
+    }
+  
+};
