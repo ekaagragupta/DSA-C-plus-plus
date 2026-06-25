285b8e4f171e4ab47124c0cd4910e2b46f4995b3
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-06-25 22:02:11 +0530

    Time: 9 ms (28.76%) | Memory: 55.1 MB (31.42%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -7,17 +7,16 @@ public:
 
         for (int i = 0; i < nums.size(); i++) {
 
-            // Ignore negative numbers and 0
             if (nums[i] <= 0)
                 continue;
 
-            // Ignore duplicates
             if (nums[i] == expected) {
                 expected++;
             }
             else if (nums[i] > expected) {
                 return expected;
             }
+            // if nums[i] < expected (duplicate), do nothing
         }
 
         return expected;
