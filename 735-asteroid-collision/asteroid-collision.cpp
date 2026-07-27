4133d8afdaf659bae356fe73241e14eb70fbfa61
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-07-27 10:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (79.62%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        std::stack<int> stack;
+        vector<int> ans; // Stores asteroids that are still alive
 
-        for (int a : asteroids) {
-            if (a > 0) {
-                stack.push(a);
-            } else {
-                while (!stack.empty() && stack.top() > 0 && stack.top() < -a) {
-                    stack.pop();
+        for (int i = 0; i < asteroids.size(); i++) {
+
+            // Positive asteroid moves right, so it cannot collide
+            // with previous asteroids. Directly store it.
+            if (asteroids[i] >= 0) {
+                ans.push_back(asteroids[i]);
+            }
+            else {
+                // Current asteroid moves left.
+                // Remove smaller right-moving asteroids from the stack.
+                while (!ans.empty() && ans.back() > 0 &&
+                       -asteroids[i] > ans.back()) {
+                    ans.pop_back();
                 }
 
-                if (stack.empty() || stack.top() < 0) {
-                    stack.push(a);
+                // If both asteroids have same size,
+                // both will explode.
+                if (!ans.empty() && ans.back() > 0 &&
+                    ans.back() == -asteroids[i]) {
+                    ans.pop_back();
+                    continue; // Current asteroid also exploded
                 }
 
-                if (!stack.empty() && stack.top() == -a) {
-                    stack.pop();
+                // Push current asteroid when it survives:
+                // - stack is empty, OR
+                // - previous asteroid also moves left
+                if (ans.empty() || ans.back() < 0) {
+                    ans.push_back(asteroids[i]);
                 }
             }
         }
 
-        std::vector<int> res(stack.size());
-        int i = stack.size() - 1;
-
-        while (!stack.empty()) {
-            res[i--] = stack.top();
-            stack.pop();
-        }
-
-        return res;        
+        return ans;
     }
 };
