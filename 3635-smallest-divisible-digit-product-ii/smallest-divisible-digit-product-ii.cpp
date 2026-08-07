d5222829605624a9d0f91ff2fa5d5326d0f3ce78
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-07 19:22:20 +0530

    Time: 35 ms (82.76%) | Memory: 36.7 MB (55.17%) - LeetSync

--- 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
+++ 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
@@ -1,133 +1,164 @@
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
+private:
+    const vector<vector<int>> kFactorCounts = {
+        {},             // 0
+        {0, 0, 0, 0},   // 1
+        {1, 0, 0, 0},   // 2
+        {0, 1, 0, 0},   // 3
+        {2, 0, 0, 0},   // 4
+        {0, 0, 1, 0},   // 5
+        {1, 1, 0, 0},   // 6
+        {0, 0, 0, 1},   // 7
+        {3, 0, 0, 0},   // 8
+        {0, 2, 0, 0}    // 9
+    };
+
+    pair<vector<int>, bool> getPrimeCount(long long t) {
+        vector<int> count(4, 0);
+        vector<int> primes = {2, 3, 5, 7};
+        for (int i = 0; i < 4; ++i) {
+            while (t % primes[i] == 0) {
+                t /= primes[i];
+                count[i]++;
+            }
+        }
+        return {count, t == 1};
+    }
+
+    vector<int> getPrimeCountFromString(const string& num) {
+        vector<int> count(4, 0);
+        for (char c : num) {
+            int d = c - '0';
+            if (d >= 1 && d <= 9) {
+                for (int i = 0; i < 4; ++i) {
+                    count[i] += kFactorCounts[d][i];
+                }
+            }
+        }
+        return count;
+    }
+
+    vector<int> getFactorCount(const vector<int>& primeCount) {
+        int c2 = primeCount[0];
+        int c3 = primeCount[1];
+        int c5 = primeCount[2];
+        int c7 = primeCount[3];
+
+        int count8 = c2 / 3;
+        int rem2 = c2 % 3;
+
+        int count9 = c3 / 2;
+        int rem3 = c3 % 2;
+
+        int count4 = rem2 / 2;
+        int count2 = rem2 % 2;
+
+        int count6 = 0;
+        if (count2 == 1 && rem3 == 1) {
+            count2 = 0;
+            rem3 = 0;
+            count6 = 1;
+        }
+        if (rem3 == 1 && count4 == 1) {
+            count2 = 1;
+            count6 = 1;
+            rem3 = 0;
+            count4 = 0;
+        }
+
+        vector<int> factors(10, 0);
+        factors[2] = count2;
+        factors[3] = rem3;
+        factors[4] = count4;
+        factors[5] = c5;
+        factors[6] = count6;
+        factors[7] = c7;
+        factors[8] = count8;
+        factors[9] = count9;
+        return factors;
+    }
+
+    int sumValues(const vector<int>& factors) {
+        int sum = 0;
+        for (int v : factors) sum += v;
+        return sum;
+    }
+
+    string construct(const vector<int>& factors) {
+        string res = "";
+        for (int digit = 2; digit <= 9; ++digit) {
+            res += string(factors[digit], '0' + digit);
+        }
+        return res;
+    }
+
 public:
     string smallestNumber(string num, long long t) {
-        int req2 = 0, req3 = 0, req5 = 0, req7 = 0;
-        long long temp = t;
-        while (temp % 2 == 0) { temp /= 2; req2++; }
-        while (temp % 3 == 0) { temp /= 3; req3++; }
-        while (temp % 5 == 0) { temp /= 5; req5++; }
-        while (temp % 7 == 0) { temp /= 7; req7++; }
-        if (temp > 1) return "-1";
+        auto [primeCount, isDivisible] = getPrimeCount(t);
+        if (!isDivisible) return "-1";
 
-        int dp[60][40];
-        for (int i = 0; i < 60; ++i) {
-            for (int j = 0; j < 40; ++j) {
-                dp[i][j] = 1e9;
-            }
-        }
-        dp[0][0] = 0;
+        vector<int> factorCount = getFactorCount(primeCount);
+        int targetLen = sumValues(factorCount);
         
-        int trans[6][2] = {{1, 0}, {0, 1}, {2, 0}, {1, 1}, {3, 0}, {0, 2}};
-        for (int i = 0; i < 60; ++i) {
-            for (int j = 0; j < 40; ++j) {
-                if (dp[i][j] == 1e9) continue;
-                for (auto& tr : trans) {
-                    int ni = min(59, i + tr[0]);
-                    int nj = min(39, j + tr[1]);
-                    dp[ni][nj] = min(dp[ni][nj], dp[i][j] + 1);
-                }
-            }
-        }
-        for (int i = 59; i >= 0; --i) {
-            for (int j = 39; j >= 0; --j) {
-                if (i < 59) dp[i][j] = min(dp[i][j], dp[i + 1][j]);
-                if (j < 39) dp[i][j] = min(dp[i][j], dp[i][j + 1]);
-            }
+        // Safe check: If required digits exceed num's length
+        if (targetLen > (int)num.length()) {
+            string ans = construct(factorCount);
+            return ans;
         }
 
-        int F2[] = {0, 0, 1, 0, 2, 0, 1, 0, 3, 0};
-        int F3[] = {0, 0, 0, 1, 0, 0, 1, 0, 0, 2};
-        int F5[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0};
-        int F7[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0};
-
-        int n = num.length();
-        bool has_zero = false;
-        int first_zero = n;
-        for (int i = 0; i < n; ++i) {
-            if (num[i] == '0') {
-                has_zero = true;
-                first_zero = i;
-                break;
+        vector<int> primeCountPrefix = getPrimeCountFromString(num);
+        size_t firstZeroIndex = num.find('0');
+        
+        if (firstZeroIndex == string::npos) {
+            bool valid = true;
+            for (int i = 0; i < 4; ++i) {
+                if (primeCountPrefix[i] < primeCount[i]) valid = false;
             }
+            if (valid) return num;
+            firstZeroIndex = num.length();
         }
 
-        if (!has_zero) {
-            int r2 = req2, r3 = req3, r5 = req5, r7 = req7;
-            for (char c : num) {
-                int d = c - '0';
-                r2 = max(0, r2 - F2[d]);
-                r3 = max(0, r3 - F3[d]);
-                r5 = max(0, r5 - F5[d]);
-                r7 = max(0, r7 - F7[d]);
-            }
-            if (r2 == 0 && r3 == 0 && r5 == 0 && r7 == 0) return num;
-        }
-
-        int limit = min(n - 1, first_zero);
-        int p2 = 0, p3 = 0, p5 = 0, p7 = 0;
-        for (int i = 0; i < limit; ++i) {
+        // Greedy scan from right to left
+        for (int i = (int)num.length() - 1; i >= 0; --i) {
             int d = num[i] - '0';
-            p2 += F2[d];
-            p3 += F3[d];
-            p5 += F5[d];
-            p7 += F7[d];
-        }
+            if (d >= 1 && d <= 9) {
+                for (int j = 0; j < 4; ++j) {
+                    primeCountPrefix[j] -= kFactorCounts[d][j];
+                }
+            }
+            
+            if (i > (int)firstZeroIndex) continue;
 
-        for (int i = limit; i >= 0; --i) {
-            int start_d = (num[i] - '0') + 1;
-            for (int d = start_d; d <= 9; ++d) {
-                int n2 = max(0, req2 - p2 - F2[d]);
-                int n3 = max(0, req3 - p3 - F3[d]);
-                int n5 = max(0, req5 - p5 - F5[d]);
-                int n7 = max(0, req7 - p7 - F7[d]);
-                int L = n - 1 - i;
+            int spaceAfterThisDigit = (int)num.length() - 1 - i;
+
+            for (int biggerDigit = d + 1; biggerDigit <= 9; ++biggerDigit) {
+                vector<int> requiredPrimes(4, 0);
+                for (int j = 0; j < 4; ++j) {
+                    requiredPrimes[j] = max(0, primeCount[j] - primeCountPrefix[j] - kFactorCounts[biggerDigit][j]);
+                }
                 
-                if (n7 + n5 + dp[n2][n3] <= L) {
-                    string ans = num.substr(0, i) + to_string(d);
-                    int rem2 = n2, rem3 = n3, rem5 = n5, rem7 = n7;
-                    for (int pos = 0; pos < L; ++pos) {
-                        for (int x = 1; x <= 9; ++x) {
-                            int nn2 = max(0, rem2 - F2[x]);
-                            int nn3 = max(0, rem3 - F3[x]);
-                            int nn5 = max(0, rem5 - F5[x]);
-                            int nn7 = max(0, rem7 - F7[x]);
-                            if (nn7 + nn5 + dp[nn2][nn3] <= L - 1 - pos) {
-                                ans += to_string(x);
-                                rem2 = nn2; rem3 = nn3; rem5 = nn5; rem7 = nn7;
-                                break;
-                            }
-                        }
-                    }
-                    return ans;
+                vector<int> factorsAfterReplacement = getFactorCount(requiredPrimes);
+                int neededSpace = sumValues(factorsAfterReplacement);
+                
+                if (neededSpace <= spaceAfterThisDigit) {
+                    int fillOnes = max(0, spaceAfterThisDigit - neededSpace); // Guarded against negatives
+                    return num.substr(0, i) + to_string(biggerDigit) + string(fillOnes, '1') + construct(factorsAfterReplacement);
                 }
-            }
-            if (i > 0) {
-                int d = num[i - 1] - '0';
-                p2 -= F2[d];
-                p3 -= F3[d];
-                p5 -= F5[d];
-                p7 -= F7[d];
             }
         }
 
-        int min_len_needed = req7 + req5 + dp[req2][req3];
-        int M = max(n + 1, min_len_needed);
-        string ans = "";
-        int rem2 = req2, rem3 = req3, rem5 = req5, rem7 = req7;
+        // If no substitution worked within the current length, expand by 1 digit safely
+        vector<int> factorsAfterExtension = getFactorCount(primeCount);
+        int neededSpace = sumValues(factorsAfterExtension);
+        int fillOnes = max(0, (int)num.length() + 1 - neededSpace); // Guarded against negatives
         
-        for (int pos = 0; pos < M; ++pos) {
-            for (int x = 1; x <= 9; ++x) {
-                int nn2 = max(0, rem2 - F2[x]);
-                int nn3 = max(0, rem3 - F3[x]);
-                int nn5 = max(0, rem5 - F5[x]);
-                int nn7 = max(0, rem7 - F7[x]);
-                if (nn7 + nn5 + dp[nn2][nn3] <= M - 1 - pos) {
-                    ans += to_string(x);
-                    rem2 = nn2; rem3 = nn3; rem5 = nn5; rem7 = nn7;
-                    break;
-                }
-            }
-        }
-        return ans;
+        return string(fillOnes, '1') + construct(factorsAfterExtension);
     }
 };
