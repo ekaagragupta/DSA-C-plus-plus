76e23286468e4e27a1eccb7ae1a718688f2502db
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-13 16:03:01 +0530

    Time: 119 ms (84.56%) | Memory: 95 MB (88.59%) - LeetSync

--- 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.cpp
+++ 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.cpp
@@ -1,69 +1,75 @@
 class Solution {
 public:
-    vector<int> longestRepeating(string s, string queryCharacters,
-                                 vector<int>& queryIndices) {
-        int n = s.size();
-        vector<int> pre(4 * n), suf(4 * n), maxLen(4 * n);
-        vector<char> leftChar(4 * n), rightChar(4 * n);
+    struct Node {
+        int val, l, r;
+        Node() : val(1), l(1), r(1) {}
+    };
 
-        auto pushUp = [&](int u, int l, int r) {
-            int mid = (l + r) >> 1;
-            int leftLen = mid - l + 1, rightLen = r - mid;
-            int left = u << 1, right = u << 1 | 1;
-            leftChar[u] = leftChar[left];
-            rightChar[u] = rightChar[right];
-            pre[u] = pre[left];
-            if (pre[left] == leftLen && rightChar[left] == leftChar[right]) {
-                pre[u] = pre[left] + pre[right];
-            }
-            suf[u] = suf[right];
-            if (suf[right] == rightLen && rightChar[left] == leftChar[right]) {
-                suf[u] = suf[right] + suf[left];
-            }
-            maxLen[u] = max(maxLen[left], maxLen[right]);
-            if (rightChar[left] == leftChar[right]) {
-                maxLen[u] = max(maxLen[u], suf[left] + pre[right]);
-            }
-        };
+    vector<Node> tree;
+    string s;
 
-        function<void(int, int, int)> build = [&](int u, int l, int r) {
-            if (l == r) {
-                pre[u] = 1;
-                suf[u] = 1;
-                maxLen[u] = 1;
-                leftChar[u] = s[l];
-                rightChar[u] = s[l];
-                return;
+    void calculate(int ind, int tl, int tr, int mid) {
+        Node& curr = tree[ind];
+        Node& left = tree[ind * 2 + 1];
+        Node& right = tree[ind * 2 + 2];
+
+        curr.val = max(left.val, right.val);
+        curr.l = left.l;
+        curr.r = right.r;
+
+        if (s[mid] == s[mid + 1]) {
+            curr.val = max(curr.val, left.r + right.l);
+
+            if (left.l == mid - tl + 1) {
+                curr.l = left.l + right.l;
             }
-            int mid = (l + r) >> 1;
-            build(u << 1, l, mid);
-            build(u << 1 | 1, mid + 1, r);
-            pushUp(u, l, r);
-        };
-
-        function<void(int, int, int, int, char)> update =
-            [&](int u, int l, int r, int pos, char ch) {
-                if (l == r) {
-                    leftChar[u] = ch;
-                    rightChar[u] = ch;
-                    return;
-                }
-                int mid = (l + r) >> 1;
-                if (pos <= mid) {
-                    update(u << 1, l, mid, pos, ch);
-                } else {
-                    update(u << 1 | 1, mid + 1, r, pos, ch);
-                }
-                pushUp(u, l, r);
-            };
-
-        build(1, 0, n - 1);
-        int k = queryIndices.size();
-        vector<int> ans(k);
-        for (int i = 0; i < k; i++) {
-            update(1, 0, n - 1, queryIndices[i], queryCharacters[i]);
-            ans[i] = maxLen[1];
+            if (right.r == tr - mid) {
+                curr.r = left.r + right.r;
+            }
         }
+    }
+
+    void build(int ind, int tl, int tr) {
+        if (tl == tr) {
+            tree[ind] = Node();
+            return;
+        }
+
+        int mid = tl + (tr - tl) / 2;
+        build(2 * ind + 1, tl, mid);
+        build(2 * ind + 2, mid + 1, tr);
+        calculate(ind, tl, tr, mid);
+    }
+
+    void update(int i, char ch, int tl, int tr, int ind) {
+        if (tl == tr) {
+            s[i] = ch;
+            tree[ind] = Node();
+            return;
+        }
+
+        int mid = tl + (tr - tl) / 2;
+        if (i <= mid)
+            update(i, ch, tl, mid, 2 * ind + 1);
+        else
+            update(i, ch, mid + 1, tr, 2 * ind + 2);
+
+        calculate(ind, tl, tr, mid);
+    }
+
+    vector<int> longestRepeating(string s, string queryCharacters, vector<int>& queryIndices) {
+        int n = s.size();
+        this->s = s;
+        tree.resize(4 * n);
+
+        build(0, 0, n - 1);
+        vector<int> ans;
+
+        for (int i = 0; i < queryIndices.size(); ++i) {
+            update(queryIndices[i], queryCharacters[i], 0, n - 1, 0);
+            ans.push_back(tree[0].val);
+        }
+
         return ans;
     }
 };
