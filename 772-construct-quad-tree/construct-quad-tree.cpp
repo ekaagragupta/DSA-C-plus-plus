895a4f057cbd87a8bf4561e56ac3bcfd863abd52
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-23 17:22:13 +0530

    Time: 12 ms (34.78%) | Memory: 18.7 MB (85.74%) - LeetSync

--- 772-construct-quad-tree/construct-quad-tree.cpp
+++ 772-construct-quad-tree/construct-quad-tree.cpp
@@ -1,27 +1,67 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
 class Solution {
- public:
-  Node* construct(vector<vector<int>>& grid) {
-    return helper(grid, 0, 0, grid.size());
-  }
-
- private:
-  Node* helper(const vector<vector<int>>& grid, int i, int j, int w) {
-    if (allSame(grid, i, j, w))
-      return new Node(grid[i][j], true);
-
-    Node* node = new Node(true, false);
-    node->topLeft = helper(grid, i, j, w / 2);
-    node->topRight = helper(grid, i, j + w / 2, w / 2);
-    node->bottomLeft = helper(grid, i + w / 2, j, w / 2);
-    node->bottomRight = helper(grid, i + w / 2, j + w / 2, w / 2);
-    return node;
-  }
-
-  bool allSame(const vector<vector<int>>& grid, int i, int j, int w) {
-    return all_of(begin(grid) + i, begin(grid) + i + w,
-                  [&](const vector<int>& row) {
-      return all_of(begin(row) + j, begin(row) + j + w,
-                    [&](int num) { return num == grid[i][j]; });
-    });
-  }
+public:
+    bool isallsame(int x,int y,vector<vector<int>>& grid,int n){
+        int val=grid[x][y];
+        for(int i=x;i<x+n;i++){
+            for(int j=y;j<y+n;j++){
+                if(grid[i][j]!=val)
+                return false;
+            }
+        }
+        return true;
+    }
+    Node* solve(int x,int y,vector<vector<int>>& grid,int n){
+        if(isallsame(x,y,grid,n)){
+            return new Node(grid[x][y],true);
+        }
+            Node* root=new Node(1,false);
+            root->topLeft=solve(x,y,grid,n/2);
+            root->topRight=solve(x,y+n/2,grid,n/2);
+            root->bottomLeft=solve(x+n/2,y,grid,n/2);
+            root->bottomRight=solve(x+n/2,y+n/2,grid,n/2);
+        return root;
+    }
+    Node* construct(vector<vector<int>>& grid) {
+        return solve(0,0,grid,grid.size());
+    }
 };
