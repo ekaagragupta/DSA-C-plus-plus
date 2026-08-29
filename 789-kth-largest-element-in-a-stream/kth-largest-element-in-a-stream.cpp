110494b96c62760aaadb5be6e7ad69ff5fa29b7c
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-29 15:58:29 +0530

    Time: 2 ms (92.89%) | Memory: 33 MB (62.05%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,13 @@
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (int x : nums) add(x);
+    }
+    int add(int val) {
+        pq.push(val);
+        if ((int)pq.size() > k) pq.pop();
+        return pq.top();
+    }
+};
